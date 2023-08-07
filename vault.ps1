4da3e58ef02a30851a4f779e6548b9692fe776ab
Mehrnoush <52617673+Mehrnoushfzln@users.noreply.github.com>
2023-08-07 20:39:18 +0100

    Create vault.ps1

---
+++ vault.ps1
@@ -0,0 +1,13 @@
+New-AzRecoveryServicesVault
+   -Name MehrnoushVault
+   -ResourceGroupName Mehrnoush_DR
+   -Location east us
+   [-Tag <Hashtable>]
+   [-DisableClassicAlerts <Boolean>]
+   [-DisableAzureMonitorAlertsForJobFailure <Boolean>]
+   [-PublicNetworkAccess <PublicNetworkAccess>]
+   [-ImmutabilityState <ImmutabilityState>]
+   [-DefaultProfile <IAzureContextContainer>]
+   [-WhatIf]
+   [-Confirm]
+   [<CommonParameters>]
